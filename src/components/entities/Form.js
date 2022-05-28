0abd9b1efb209dd2ec744bdced4198e21f146771
parfenovyura <parfenovyura@inbox.ru>
2022-05-28 21:47:10 +0500

    форматирование кода

--- src/components/entities/Form.js
+++ src/components/entities/Form.js
@@ -2,46 +2,32 @@ import React, { useState } from "react";
 import Imprint from "./Imprint";
 
 const Form = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [selectValue, setSelectValue] = useState('Именительный');
 
-    const [inputValue, setInputValue] = useState(``);
-    const [selectValue, setSelectValue] = useState(`Именительный`);
-
-    return (
-        <div>
-
-            <form action="#" method="POST">
-
-                <p>Введите слово на русском языке в именительном падеже, единственном числе</p>
-
-                <p>
-                    <input type="text" name="enter" value={inputValue}
-                        onChange={e => setInputValue(e.target.value)}
-                    />
-                </p>
-
-                <p>Выберите падеж</p>
-
-                <p>
-                    <select name="case[]" value={selectValue}
-                        onChange={e => setSelectValue(e.target.value)}
-                    >
-                        <option value="Именительный" selected>Именительный</option>
-                        <option value="Родительный">Родительный</option>
-                        <option value="Дательный">Дательный</option>
-                        <option value="Винительный">Винительный</option>
-                        <option value="Творительный">Творительный</option>
-                        <option value="Предложный">Предложный</option>
-                    </select>
-                </p>
-
-
-                <p>Слово в выбранном падеже:  </p>
-                <Imprint word={inputValue} caseOfWord={selectValue} />
-
-            </form>
-
-        </div>
-    )
+  return (
+    <div>
+      <form action="#" method="POST">
+        <p>Введите слово на русском языке в именительном падеже, единственном числе</p>
+        <p>
+          <input type="text" name="enter" value={inputValue} onChange={e => setInputValue(e.target.value)}/>
+        </p>
+        <p>Выберите падеж</p>
+        <p>
+          <select name="case[]" value={selectValue} onChange={e => setSelectValue(e.target.value)}>
+            <option value="Именительный" selected>Именительный</option>
+            <option value="Родительный">Родительный</option>
+            <option value="Дательный">Дательный</option>
+            <option value="Винительный">Винительный</option>
+            <option value="Творительный">Творительный</option>
+            <option value="Предложный">Предложный</option>
+          </select>
+        </p>
+        <p>Слово в выбранном падеже:  </p>
+        <Imprint word={inputValue} caseOfWord={selectValue} />
+      </form>
+    </div>
+  )
 };
 
 export default Form;
--- src/components/entities/Imprint.js
+++ src/components/entities/Imprint.js
@@ -1,22 +1,10 @@
 import React from "react";
 import getDeclination from "./getDeclination";
 
-
 const Imprint = props => {
-
     const { word, caseOfWord } = props;
-
-
     const declination = getDeclination(word, caseOfWord);
-
-
-    return (
-        <div>
-            <p>{declination}</p>
-        </div>
-    )
+    return (<p>{declination}</p>)
 };
-
-
 
 export default Imprint;
